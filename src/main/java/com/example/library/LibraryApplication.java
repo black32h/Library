2f5b979f7e3da270120d6411be3cf82377449df6
black32h <blackspetya@gmail.com>
2025-02-15 12:04:58 +0200

    Update LibraryApplication.java

--- src/main/java/com/example/library/LibraryApplication.java
+++ src/main/java/com/example/library/LibraryApplication.java
@@ -3,11 +3,11 @@ package com.example.library;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// Основний клас для запуску Spring Boot програми
+@SpringBootApplication  // Анотація для позначення класу як точки входу в Spring Boot додаток
 public class LibraryApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(LibraryApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        // Запускаємо Spring Boot додаток
+        SpringApplication.run(LibraryApplication.class, args);
+    }
 }
